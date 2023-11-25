fab1dbd6c1f9169f94fe4ea97664884665da9e3e
srivardhan <srivardhanpusala18@gmail.com>
2023-11-25 20:50:12 +0530

    add new changes

--- src/navbar.js
+++ src/navbar.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import './App.css'; // Import your CSS file for styling
 import image5 from './Images/sri.jpg'
 
+const openLink = (name) => {
+  const Linkedin = 'https://www.linkedin.com/in/sri-vardhan-pusala-8412871a4/';
+  const GitHub = 'https://github.com/Sri-vardhan18/';
+
+  return () => {
+    if (name === 'Linkedin') {
+      window.open(Linkedin, '_blank');
+    } else if (name === 'GitHub') {
+      window.open(GitHub, '_blank');
+    }
+  };
+};
 
 const Navbar = () => {
   return (
     <div className="navbar">
+      <ul className='containers'>
+        <li onClick={openLink('Linkedin')}>Linkedin</li>
+        <li onClick={openLink('GitHub')}>Github</li>
+      </ul>
       <div className="profile">
         <img src={image5} alt="Profile" />
       </div>
